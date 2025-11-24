c8f805cb4f20d3de5ff395819774fdff4c2492f4
felipeflosii <luizfelipeflosi@gmail.com>
2025-11-24 15:01:14 -0300

    fix: corrige bug crítico (#002)

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -3,12 +3,12 @@ import type { Noticia } from "../types/gnews";
 export const fetchNoticias = async (): Promise<Noticia[]> => {
   try {
     const response = await fetch(
-      "https://gnews.io/api/v4/top-headlines?topic=technology&lang=pt&token=ea926c3e5b6b648f16e5ad3517526f82"
+      `https://gnews.io/api/v4/top-headlines?topic=technology&lang=pt&token=${import.meta.env.VITE_GNEWS_API_KEY}`
     );
     const data = await response.json();
     return data.articles || [];
-  } catch (error) {
-    console.error("Erro ao buscar notícias:", error);
+  } catch (err) {
+    console.error("Erro ao buscar notícias:", err);
     return [];
   }
 };
